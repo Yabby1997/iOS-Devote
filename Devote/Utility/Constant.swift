9369e74562b3e36a62c226863a766d328d02dc10
seunghun yang <yabby1997@gmail.com>
2021-03-30 19:50:05 +0900

    53. 2. Introduction to Core Data and understand the Xcode's Core Data template

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Seunghun Yang on 2021/03/30.
+//
+
+import SwiftUI
+
+// MARK: - Formatter
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
